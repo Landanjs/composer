24c6eef84b38c9fb271eed12d145c438864509ff
Landan Seguin <landanjs@gmail.com>
2022-04-11 14:11:52 -0700

    Add channel dim to target

--- composer/models/deeplabv3/deeplabv3.py
+++ composer/models/deeplabv3/deeplabv3.py
@@ -196,6 +196,7 @@ class ComposerDeepLabV3(ComposerModel):
 
     def loss(self, outputs: Any, batch: BatchPair):
         target = batch[1]
+        target = target.unsqueeze(1)
         loss = self.dice_loss(outputs, target)
         #loss = soft_cross_entropy(outputs, target, ignore_index=-1)  # type: ignore
         return loss
