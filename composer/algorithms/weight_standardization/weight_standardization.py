85c755302489aa32c50b0369b8ce923a6732e059
Landan Seguin <landanjs@gmail.com>
2022-09-19 20:23:07 +0000

    Ignore bias

--- composer/algorithms/weight_standardization/weight_standardization.py
+++ composer/algorithms/weight_standardization/weight_standardization.py
@@ -15,13 +15,19 @@ __all__ = ['apply_weight_standardization', 'WeightStandardization']
 def _standardize_weights(W: torch.Tensor):
     reduce_dims = list(range(1, W.dim()))
     W_var, W_mean = torch.var_mean(W, dim=reduce_dims, keepdim=True, unbiased=False)
-    return (W - W_mean) / (torch.sqrt(W_var + 1e-5))
+    return (W - W_mean) / (torch.sqrt(W_var + 1e-10))
 
 
 class WeightStandardizer(nn.Module):
 
     def forward(self, W):
         return _standardize_weights(W)
+
+
+class ZeroBias(nn.Module):
+
+    def forward(self, B):
+        return B - B
 
 
 def apply_weight_standardization(model: torch.nn.Module, n_last_layers_ignore: bool = False):
@@ -34,6 +40,7 @@ def apply_weight_standardization(model: torch.nn.Module, n_last_layers_ignore: b
         if (isinstance(module, nn.Conv1d) or isinstance(module, nn.Conv2d) or
                 isinstance(module, nn.Conv3d)) or isinstance(module, nn.Linear):
             parametrize.register_parametrization(module, 'weight', WeightStandardizer())
+            parametrize.register_parametrization(module, 'bias', ZeroBias())
             count += 1
 
     target_ws_layers = count - n_last_layers_ignore
@@ -44,6 +51,7 @@ def apply_weight_standardization(model: torch.nn.Module, n_last_layers_ignore: b
         if (isinstance(module, nn.Conv1d) or isinstance(module, nn.Conv2d) or
                 isinstance(module, nn.Conv3d)) or isinstance(module, nn.Linear):
             parametrize.remove_parametrizations(module, 'weight', leave_parametrized=False)
+            parametrize.remove_parametrizations(module, 'bias', leave_parametrized=False)
             count -= 1
 
     return count
